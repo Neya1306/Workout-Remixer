904241f63cde47eee8996ae5f8013bee12b4b944
Brian Cheruiyot <47404512+littbrian@users.noreply.github.com>
2024-04-14 03:09:39 +0000

    Implemented routine creation

--- App/models/routine.py
+++ App/models/routine.py
@@ -3,13 +3,11 @@ from App.database import db
 class Routine(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    workout_id = db.Column(db.Integer, db.ForeignKey('workout.id'), nullable=False)
-    workout = db.relationship('Workout', backref=db.backref('routines', lazy=True))
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    user = db.relationship('User', backref=db.backref('routines', lazy=True))
+  
 
     def __init__(self, name):
         self.name = name
+
  
 
 
@@ -17,8 +15,7 @@ class Routine(db.Model):
         return {
             "id": self.id,
             "name": self.name,
-            "user_id": self.user_id,
-            "Workouts": [workout.to_dict() for workout in self.workouts]
+            
         }
 
   
