0370a20a55ff49a271e8da7b868943c5d294c82a
CryMich <michellekhadan@gmail.com>
2024-04-19 10:04:22 +0000

    Major Changes to Front End

--- App/static/main.js
+++ App/static/main.js
@@ -14,9 +14,32 @@ function loadTable(users){
     }
 }
 
+//code for slides
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("slide");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  slides[slideIndex-1].style.display = "block";  
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
+}
+
+function adjustContainerHeight() {
+    var container = document.getElementById('result');
+    container.style.height = container.scrollHeight + 'px';
+}
+
 async function main(){
     const users = await getUserData();
     loadTable(users);
+    showSlides(); // Call the showSlides function
+    adjustContainerHeight();
 }
 
 main();
